566f0467c6e6317abb36740c18fa49b0e3f6357d
Amir Avkhadiev <hasselbaink@protonmail.com>
2016-10-26 13:45:00 +0300

    'replace_str.py' is work now

--- replace_str.py
+++ replace_str.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 # this code don't work, but it will be use in future versions
-line = ''
-words = []
-output = []
-with open('words.txt', mode='r') as f:
-	for line in f:
-		words = line.split(':')
-		output.append(words[1]+':'+words[0])
-with open('words.txt', mode='a') as f:
-	for w in output:
-		f.write(w)
+def start(path='words.txt'):
+	input = []
+	output = []
+	line = ''
+	with open('words.txt', mode='r') as f:
+		for line in f:
+			if line.find(':') == -1:
+				continue
+			else:
+				input = line.split(':')
+				output.append(input[1][:-1]+':'+input[0]+'\n')
+	with open('words.txt', mode='w') as f:
+		for w in output:
+			f.write(w)
+import sys
+args = sys.argv
+if len(args) == 1:
+	start()
+elif len(args) == 2:
+	if args[1] == '-h' or args[1] == '--help':
+		pass
+	else:
+		start(path=args[1])
